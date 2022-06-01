84380e3e6729bdb2c4d8acc9c651a951da5323ba
JesusHT <102433533+JesusHT@users.noreply.github.com>
2022-06-01 17:53:03 -0500

    Blog2Watch v1.0
    Blog terminado funcional 100% de manera local

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,12 @@
 <?php
 
   $server = 'localhost';
-  $username = 'root';
-  $password = '!JesusHT12015';
-  $database = 'database';
-  //$username = 'id19025691_root';
-  //$password = '!Root1234567'; 
-  //$database = 'id19025691_blog2watch';
+ // $username = 'root';
+ // $password = '!JesusHT12015';
+//  $database = 'database';
+  $username = 'id19025691_root';
+  $password = '!Root1234567'; 
+  $database = 'id19025691_blog2watch';
 
   try {
     $conn = new PDO("mysql:host=$server;dbname=$database;", $username, $password);
